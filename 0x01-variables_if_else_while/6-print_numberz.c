17f9c6a61efef55d1a6f86a4757e4ca1df3a440e
Cristopher Colin <98349058+CColinn90@users.noreply.github.com>
2022-02-04 20:55:45 -0600

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,8 +9,10 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n < 10; n++)
-		printf("%d", n);
-	printf("\n");
-	return (0);
+	for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
 }
