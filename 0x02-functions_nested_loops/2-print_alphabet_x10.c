18315367a4beb5cbe4c9975dc604ef43306f19af
Cristopher Colin <98349058+CColinn90@users.noreply.github.com>
2022-02-08 21:27:17 -0600

    Create 2-print_alphabet_x10.c
    Function that prints 10 times the alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+*print_alphabet_x10 - entry point
+*
+*function name - putchar
+*
+*Description - Void = no value
+*
+*Return: 0 - Success
+*
+*/
+
+void print_alphabet_x10(void)
+{
+char a = 'a';
+
+int b = 1;
+
+for (a = 1; a <= 10; a++)
+{
+for (b = 'a'; b <= 'z'; b++)
+_putchar(c);
+_putchar('\n');
+}
+
+}
